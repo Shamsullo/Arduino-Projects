f289360422035daaf7dd5a2aa5c24881f41de8bb
Shamsullo <34841262+Shamsullo@users.noreply.github.com>
2018-11-12 16:49:35 +0600

    Update sketch_oct29a.ino

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -1,99 +1,70 @@
 
-#include <Servo.h> 
-#include <SoftwareSerial.h>
+  #include <Servo.h> 
+  #include <SoftwareSerial.h>
 
-SoftwareSerial BT(3,2);
-Servo rightServo;        // create servo object to control a servo 
-Servo leftServo;                // a maximum of eight servo objects can be created 
- 
-void fw();
-void bw();
-void left();
-void right();
-void stop();
- 
-void setup() 
-{ 
-  // put your setup code here, to run once:
-  BT.begin(9600);
-  Serial.begin(9600);
-  pinMode(3,OUTPUT);
-  pinMode(5,OUTPUT);
-  
-  rightServo.attach(3);  // attaches the servo on pin 9 to the servo object 
-  leftServo.attach(5);
+  SoftwareSerial BT(3,2);
+  Servo rightServo;        // create servo object to control a servo 
+  Servo leftServo;                // a maximum of eight servo objects can be created 
    
-} 
- 
-void loop() 
-{ 
+  void setup() { 
+    // put your setup code here, to run once:
+    BT.begin(9600);
+    Serial.begin(9600);
+    pinMode(3,OUTPUT);
+    pinMode(5,OUTPUT);
+    
+    rightServo.attach(3);  // attaches the servo on pin 9 to the servo object 
+    leftServo.attach(5);
+     
+  } 
+   
+  void loop() { 
     if(BT.available()){
       char input = BT.read();
       Serial.println(input);
-      
-      int RL = 0;
-      
-      if( input == 'U'){
-        fw();
-        RL = 1;
-        if (input == 'u')
+      switch(input){
+        case 'U':
+          fw();
+          break;
+        case 'D':
+          bw();
+          break;
+        case 'L':
+          left();
+          break;
+        case 'R':
+          right();
+          break;
+        default:
           stop();
-      }else if( input == 'D'){
-        bw();
-        RL = -1;
-        if (input == 'd')
-          stop();
+          break;
       }
-      
-      if( input == 'R'){
-        right();
-        if (input == 'r'){
-          if (RL == 1){
-            fw();
-          }else if (RL == -1){
-            bw();
-        }
-      }else if( input == 'L'){
-        left();
-           if (input == 'r'){
-          if (RL == 1){
-            fw();
-          }else if (RL == -1){
-            bw();
-        }
-      }
-      
-      if (intput == 'U'){
-        stop();
-      }
-        
-
-} 
+    }     
+  } 
 
 
-void fw(){
+  void fw(){
     rightServo.write(0);
     leftServo.write(180);
-}
+  } 
 
-void bw(){
+  void bw(){
     rightServo.write(180);
     leftServo.write(0);
 
-}
+  }
 
-void left(){
+  void left(){
     rightServo.write(0);
-    leftServo.write(90);
-}
+    leftServo.write(91);
+  }
 
-void right(){
-    rightServo.write(90);
+  void right(){
+    rightServo.write(91);
     leftServo.write(180);
-}
+  }
 
-void stop(){
-    rightServo.write(90);
-    leftServo.write(90);
-}
-
+  void stop(){
+    rightServo.write(91);
+    leftServo.write(91);
+  }
